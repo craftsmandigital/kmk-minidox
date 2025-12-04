dfe9cd691ec21f75fd4261b0b46ca6cf8c072dad
craftsmandigital <htcjon10@gmail.com>
2025-12-04 18:12:04 +0100

    Added a SYM layer and a key to reach it. When hold it is on sym layer.Wen pressed it is OneShot

--- code.py
+++ code.py
@@ -8,21 +8,25 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.split import Split, SplitType, SplitSide
 from kmk.modules.layers import Layers
+from kmk.modules.sticky_keys import StickyKeys
 
 # from kmk.modules.macros import Macros, Press, Release, Tap
 from kmk.modules.macros import Macros
 
 
+# 1. Import our new, custom OSL module
+#from osl import OSL
 import hardware  # Pins (Local Library)
 
 
 # --- INITIAL SETUP ---
 keyboard = KMKKeyboard()
 keyboard.modules.append(Layers())
-
+keyboard.modules.append(StickyKeys())
 # Enable Macros
 # macros = Macros()
 keyboard.modules.append(Macros())
+#keyboard.modules.append(OSL())  # This enables our custom KC.OSL
 
 # --- 1. JUMPER DETECTION ---
 jumper = digitalio.DigitalInOut(hardware.SIDE_DETECTION_PIN)
@@ -63,7 +67,15 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 import layout    # Custom Keys (Nordic + Dead Fixes)  (Local Library)
 
 # --- 4. KEYMAP ---
+
+# --- 4. KEYMAP ---
+_BASE = 0
+_SYM  = 1
+
 keyboard.keymap = [
+    # -------------------------------------------------------------------------
+    # LAYER 0: BASE
+    # -------------------------------------------------------------------------
     [
         # Left Hand (Cols 1-5)                     # Right Hand (Cols 1-5)
         KC.Q,  KC.W,  KC.E,    KC.R,    KC.T,      KC.Y,   KC.U,    KC.I,    KC.O,   KC.P,    # Row 1
@@ -71,11 +83,39 @@ keyboard.keymap = [
         KC.Z,  KC.X,  KC.C,    KC.V,    KC.B,      KC.N,   KC.M,    layout.US_QUOT,  layout.NO_AA,  layout.NO_AE,   # Row 3 
         
         # Left Hands Thumbs (Cols 3-5)             # Right Hands Thumbs (Cols 3-5)
-        KC.NO, KC.NO, KC.LCTL, KC.LSFT, KC.SPC,    KC.ENT, KC.BSPC, KC.LALT, KC.NO,  KC.NO,   # Row 4
+        # Note: Left Inner (SPC) is now LT(_SYM, KC.SPC)
+        KC.NO, KC.NO, KC.LCTL, KC.SPC, KC.SK(KC.MO(_SYM)),    KC.ENT, KC.BSPC, KC.LALT, KC.NO,  KC.NO,   # Row 4
+    ],
+
+    # -------------------------------------------------------------------------
+    # LAYER 1: SYM (Miryoku Style)
+    # -------------------------------------------------------------------------
+    [
+        # Left Hand (Cols 1-5)                     # Right Hand (Cols 1-5)
+
+        # +        !        ;        '        *        ,        .        CW       Caps     NO
+        KC.PLUS, KC.EXLM, KC.SCLN, layout.US_QUOT, KC.ASTR,   KC.COMM, KC.DOT,  KC.CW,   KC.CAPS, KC.NO,   # Row 1
+
+        
+        # -        ?        :        "        /        {        (        [        $        _
+        KC.MINS, KC.QUES, KC.COLN, layout.US_DQUO, KC.SLSH,   KC.LCBR, KC.LPRN, KC.LBRC, KC.DLR,  KC.UNDS, # Row 2
+        
+        # %        ~        ^        `        =        }        )        ]        &        #
+        KC.PERC, layout.US_TILD, layout.US_CIRC, layout.US_GRV, KC.EQL,    KC.RCBR, KC.RPRN, KC.RBRC, KC.AMPR, KC.HASH, # Row 3 
+        
+        # Left Hands Thumbs (Cols 3-5)             # Right Hands Thumbs (Cols 3-5)
+
+        # |        \        TRNS (Hold)            >        @        NO
+        KC.NO, KC.NO, KC.PIPE, KC.BSLS, KC.TRNS,             KC.RABT, KC.AT,   KC.NO,   KC.NO,  KC.NO,   # Row 4
     ]
 ]
 
+
+
+
 if __name__ == '__main__':
     keyboard.go()
+
+
 
 
